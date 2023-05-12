532c73ff85b25a53ffa369548576f562cce09305
yxm <yxm.dev@gmx.ie>
2023-05-12 17:36:16 -0300

    ...

---
+++ README.md
@@ -0,0 +1,1 @@
+`pm.sh` intends to be a universal package manager with display in `fzf` ([link](https://github.com/junegunn/fzf)).
---
+++ pm.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+function pm(){
+    OS=$(lsb_release -is)
+        if [[ "$1" == "-i" ]] || [[ "$1" == "--install" ]]; then
+             if [[ "OS" == "Arch" ]] || [[ -z "$2" ]]; then
+                pacman -Slq | fzf --multi --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S
+             elif [[ "OS" == "Arch" ]] || [[ -n "$2" ]]; then
+                pacman -S $2
+             elif [[ "OS" == "Ubuntu" ]] || [[ -z "$2" ]]; then
+                 sudo apt-get install | fzf --multi --preview 'sudo apt-get install {1}' | xargs -ro sudo apt-get install
+             elif [[ "OS" == "Ubuntu" ]] || [[ -n "$2" ]]; then
+                 sudo apt-get install $2
+        fi
+        elif [[ "$1" == "-r" ]] || [[ "$1" == "--remove" ]]; then
+            if [[ -z "$2" ]]; then
+                pacman -Qq | fzf --multi --preview 'pacman -Qi {1}' | xargs -ro sudo pacman -Runs
+            else
+                pacman -Runs $2
+            fi
+        elif [[ "$1" == "-u" ]] || [[ "$1" == "--update" ]]; then
+            pacman -Syu
+        else
+            echo "Option not defined."
+        fi
+    }
+    alias pmi="pm -i"
+    alias pmr="pm -r"
+    alias pmu="pm -u"
+
