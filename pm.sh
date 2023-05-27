d247338f3e1ef3623eb6ee97644b13471c54d031
yxm <yxm.dev@gmx.ie>
2023-05-26 22:07:03 -0300

    ...

--- pm.sh
+++ pm.sh
@@ -4,8 +4,10 @@ function pm(){
     OS=$(lsb_release -is)
         if [[ "$1" == "-i" ]] || [[ "$1" == "--install" ]]; then
              if [[ "OS" == "Arch" ]] || [[ -z "$2" ]]; then
+                rm /var/lib/pacman/db.lck
                 pacman -Slq | fzf --multi --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S
              elif [[ "OS" == "Arch" ]] || [[ -n "$2" ]]; then
+                rm /var/lib/pacman/db.lck
                 pacman -S $2
              elif [[ "OS" == "Ubuntu" ]] || [[ -z "$2" ]]; then
                  sudo apt-get install | fzf --multi --preview 'sudo apt-get install {1}' | xargs -ro sudo apt-get install
@@ -14,11 +16,14 @@ function pm(){
         fi
         elif [[ "$1" == "-r" ]] || [[ "$1" == "--remove" ]]; then
             if [[ -z "$2" ]]; then
+                rm /var/lib/pacman/db.lck
                 pacman -Qq | fzf --multi --preview 'pacman -Qi {1}' | xargs -ro sudo pacman -Runs
             else
+                rm /var/lib/pacman/db.lck
                 pacman -Runs $2
             fi
         elif [[ "$1" == "-u" ]] || [[ "$1" == "--update" ]]; then
+            rm /var/lib/pacman/db.lck
             pacman -Syu
         else
             echo "Option not defined."
